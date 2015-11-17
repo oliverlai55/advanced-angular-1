9d75699a712649e55fe8cbb28b3a7588dcd69261
oliverlai55 <oliverlai55@gmail.com>
2015-11-17 14:50:44 -0500

    Using Routing on AngularJS

---
+++ controller.js
@@ -0,0 +1,43 @@
+var countryApp = angular.module('countryApp',['ngRoute']);
+
+countryApp.config(function($routeProvider){
+	$routeProvider.when('/',{
+		templateUrl: 'country-list.html',
+		controller: 'countryCntrl'		
+	}).
+	when('/:countryName/:population',{
+		templateUrl: 'country-detail.html',
+		controller: 'countryDetailCntrl'
+	}).
+	when('/:countryName',{
+		templateUrl: 'country-detail.html',
+		controller: 'countryDetailCntrl'
+	}).
+	otherwise({
+		redirectTo: '/'
+	});
+});
+
+countryApp.controller('countryCntrl', function($scope, $http){
+	$http.get('countries.json').success(function(countryData){
+		$scope.countries = countryData;
+	});
+});
+
+countryApp.controller('countryDetailCntrl', function($scope, $routeParams, $http){
+	$scope.name = $routeParams.countryName;
+	$http.get('countries.json').success(function(countryDetailData){
+		var country = countryDetailData.filter(function(currCountry){
+			return currCountry.name === $scope.name;
+		})[0];
+		console.log(country);
+		$scope.country = country;
+
+	});
+});
+
+
+
+
+
+
